3cc9cbca6ac2ab6a3a612e3e31c59e184ddf04e3
kkvm <kishoreortonn@gmail.com>
2024-01-09 20:54:54 +0530

    Commit

--- Collections/Dictionary.py
+++ Collections/Dictionary.py
@@ -5,37 +5,36 @@
 # get(),keys(),items(),update(),thisdict(),thisdict.pop()
 
 # Print the values
-a =	{
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
+a = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
 }
 print(a)
 
 # Print the specific value by keys
 b = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
 }
 print(b["brand"])
 
 # Will not allow duplicates so last value will print
 c = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964,
-  "year": 2020
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964,
+    "year": 2020
 }
 print(c)
 
 # Multiple values can be given
 d = {
-  "brand": "Ford",
-  "electric": False,
-  "year": 1964,
-  "colors": ["red", "white", "blue"]
+    "brand": "Ford",
+    "electric": False,
+    "year": 1964,
+    "colors": ["red", "white", "blue"]
 }
 
 print(d)
-
