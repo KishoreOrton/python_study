6b5b142eb386258e23fc20ec0a63c90305accb8d
kkvm <kishoreortonn@gmail.com>
2024-01-10 22:02:52 +0530

    Commit

--- Function/Function_Addition.py
+++ Function/Function_Addition.py
@@ -1,8 +1,9 @@
-def add(n1,n2):
-    return n1+n2    # Store the return value in a variable so assigned to Addition variable
+def add(n1, n2):
+    return n1 + n2  # Store the return value in a variable so assigned to Addition variable
+
 
 number1 = int(input("Enter number 1 : "))
 number2 = int(input("Enter number 2 : "))
 
-Addition = add(number1,number2)
+Addition = add(number1, number2)
 print(Addition)
